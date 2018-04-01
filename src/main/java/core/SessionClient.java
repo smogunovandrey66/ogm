a271cf3888639f223f9ab33aa05b66ee3f9bdfce
smogunov66 <Hkb5KR7>
2018-04-01 23:14:57 +0500

    1

--- src/main/java/core/SessionClient.java
+++ src/main/java/core/SessionClient.java
@@ -17,6 +17,8 @@ public class SessionClient{
     private int posBuffer;
     private InputStream inputStream;
     private OutputStream outputStream;
+    private int size;
+    private boolean useCrc16;
     public SessionClient(String host, int port) throws IOException {
         socket = new Socket(host, port);
         inputStream = socket.getInputStream();
@@ -24,9 +26,22 @@ public class SessionClient{
     }
     public void send(byte[] command){
         try {
+            size = command.length & 0xFFFF;
+            outputStream.write(size >> 8);
+            outputStream.write(size & 0xFF);
             outputStream.write(command);
+            if(useCrc16){
+
+            }
+            inputStream.read(buffer, 0, 3);
+            size = buffer[0] << 8 + buffer[1];
+            //Неверная команда
+            if(buffer[2] != command[0]){
+
+            }
+
         } catch (IOException e) {
-            ;
+            logger.error(e.getMessage());
         }
     }
 
