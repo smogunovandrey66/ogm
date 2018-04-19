50eeb0066db4fe0db2175b435cd4d2ba7d94b78f
smogunov66 <Hkb5KR7>
2018-04-20 00:31:10 +0500

    1

--- src/main/java/localserver/LocalBlock.java
+++ src/main/java/localserver/LocalBlock.java
@@ -1,9 +1,22 @@
 package localserver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class LocalBlock {
-    private String path;
+    public static final String BLOCK_PROPERTIES = "block.properties";
+    public static final String ID_EQ = "idEq";
+    public static final String SW_EQ = "swEq";
+    public static final Path DEFAULT_PATH= Paths.get(System.getProperty("user.home"), "ogm", "localblocks");
+
+    private String pathToBlock;
     private byte id;
     private byte sw;
     private ArrayList<String> strInfo;
@@ -41,7 +54,26 @@ public class LocalBlock {
             localUdpServer = new LocalUdpServer(this);
     }
 
-    public static void init(byte idEq){
+    public static void makeDeaultBlock(byte idEq, byte swEq, Path pathToBlock) throws IOException {
+        Files.createDirectories(pathToBlock);
+        Path pathToSettings = pathToBlock.resolve(BLOCK_PROPERTIES);
+        Files.createFile(pathToSettings);
+        Properties properties = new Properties();
+        properties.setProperty(ID_EQ, String.valueOf(idEq & 0xFF));
+        properties.setProperty(SW_EQ, String.valueOf(swEq & 0xFF));
+        FileOutputStream fileOutputStream = new FileOutputStream(pathToSettings.toFile());
+        properties.store(fileOutputStream, null);
+        fileOutputStream.close();
+    }
 
+    public static void main(String[] args) {
+        byte idEq = (byte) 140;
+        byte swEq = (byte) 0;
+        Path pathToBlock = DEFAULT_PATH.resolve(String.valueOf(idEq & 0xFF));
+        try {
+            makeDeaultBlock(idEq, swEq, pathToBlock);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
